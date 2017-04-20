749b5d031f7528efa79ec43c7c11584dc86e7739
zachh20 <zachh20@msn.com>
2017-04-19 19:11:14 -0700

    part way done!

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -2,27 +2,28 @@
 
 # Read in the data
 pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
-
+is.data.frame(pulitzer)
 # Install and load the needed libraries
 # Be sure to comment out the install.packages function so it won't install it every time it runs
 # Remeber you only need to install a package once
-#install.packages(dplyr)
+install.packages(dplyr)
 library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
-
+colnames(pulitzer)
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
-
+str(pulitzer)
+# A lot of the numbers are characters and not int, I see why for the change in daily circulation so they can include the +/- and % at the end but im not sure why for the actual ciriculation numbers
 
 # Add a column in a dataframe called 'Pulitzer.Prize.Change` that contains the diffrence in changes
 # in Pulitzer Prize Winners from 2004 to 2013 and Pultizer Prize Winners from 1990 to 2003.
-
+pulitzer <- mutate(pulitzer, Pulitzer.Prize.Change = Pulitzer.Prize.Winners.and.Finalists..2004.2014 - Pulitzer.Prize.Winners.and.Finalists..1990.2003)
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
